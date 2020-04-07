f31b5d3fd7416de48e1f5b68464d23e3d55894e9
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-07 17:41:44 -0400

    put fixed, router refactor. mvp done

--- data/postRouter.js
+++ data/postRouter.js
@@ -1,5 +1,99 @@
 const express = require('express');
+const db = require('./db')
 const postRouter = express.Router()
 
+// ENDPOINTS
+
+postRouter.post('/', (req, res) => {
+    const newPost = req.body
+    newPost.title == "" || newPost.contents == "" ? res.status(400).json({errorMessage: 'Title or contents missing. Please address and try again'})
+    :
+    db.insert(req.body)
+    .then(post =>{
+        res.status(201).json(post)
+    })
+    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
+})
+
+postRouter.post('/:id/comments', (req, res) => {
+    const newComment = req.body
+    newComment.text == "" || !Number.isInteger(newComment.post_id) ? res.status(400).json({errorMessage: 'Text or Post Id missing. Include text or insure that post_id is of type Integer.'})
+    :
+    db.insertComment(req.body)
+    .then(comment =>{
+        res.status(201).json(comment)
+    })
+    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
+})
+
+postRouter.get('/', (req, res) => {
+    db.find()
+    .then(posts =>{
+        res.status(200).json(posts)
+    })
+    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
+})
+
+postRouter.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(post =>{
+        if(post.length > 0){
+            return res.status(200).json(post);
+        } else {
+            return res.status(404).json({message: "Post with that id not found"});
+        }
+    })
+    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
+})
+
+postRouter.get('/:id/comments', (req, res) => {
+    db.findPostComments(req.params.id)
+    .then(post =>{
+        if(post.length > 0){
+            return res.status(200).json(post);
+        } else {
+            return res.status(404).json({message: "Post with that id not found"});
+        }
+    })
+    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
+})
+
+postRouter.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+    .then(posts => {
+        if(posts){
+            db.findById(req.params.id).then(post=>{
+                res.status(200).json(post);
+            })
+            .catch(err=>{
+                res.status(500).json({errorMessage: "The post could not be removed"})
+            })
+            res.status(200).json({message: 'This post was YEETED'})
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.' })
+        }
+    })
+});
+
+postRouter.put('/:id', (req, res) => {
+    const updatePost = req.body
+    if(updatePost.title == "" || updatePost.contents == ""){
+        return res.status(400).json({errorMessage: 'Title or contents missing. Please address and try again'})
+    } else {
+    db.update(req.params.id, updatePost)
+    .then(posts => {
+        if(posts){
+            db.findById(req.params.id).then(post=>{
+                res.status(200).json(post);
+            })
+            .catch(err=>{
+                res.status(500).json({errorMessage: "The post could not be updated"})
+            })
+        } else {
+            res.status(404).json({message: 'The post with the specified ID does not exist.' })
+        }
+    })
+    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
+}})
 
 module.exports = postRouter;
--- index.js
+++ index.js
@@ -1,117 +1,18 @@
 const express = require('express')
 
 
-// const postRouter = require('')
+const postRouter = require('./data/postRouter')
 const server = express();
-server.use(express.json());
 
-const db = require('./data/db')
+server.use(express.json());
+server.use("/api/posts", postRouter)
 
 server.get('/', (req, res) => {
     res.send(`
       <h2>Blog API</h>
-      <p>Good to Go Chief, happy coding</p>
+      <p>Good to go Chief, happy coding</p>
     `);
   });
-
-// ENDPOINTS
-
-server.post('/api/posts', (req, res) => {
-    const newPost = req.body
-    newPost.title == "" || newPost.contents == "" ? res.status(400).json({errorMessage: 'Title or contents missing. Please address and try again'})
-    :
-    db.insert(req.body)
-    .then(post =>{
-        res.status(201).json(post)
-    })
-    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
-})
-
-server.post('/api/posts/:id/comments', (req, res) => {
-    const newComment = req.body
-    newComment.text == "" || !Number.isInteger(newComment.post_id) ? res.status(400).json({errorMessage: 'Text or Post Id missing. Include text or insure that post_id is of type Integer.'})
-    :
-    db.insertComment(req.body)
-    .then(comment =>{
-        res.status(201).json(comment)
-    })
-    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
-})
-
-server.get('/api/posts', (req, res) => {
-    db.find()
-    .then(posts =>{
-        res.status(200).json(posts)
-    })
-    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
-})
-
-server.get('/api/posts/:id', (req, res) => {
-    db.findById(req.params.id)
-    .then(post =>{
-        if(post.length > 0){
-            return res.status(200).json(post);
-        } else {
-            return res.status(404).json({message: "Post with that id not found"});
-        }
-    })
-    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
-})
-
-server.get('/api/posts/:id/comments', (req, res) => {
-    db.findPostComments(req.params.id)
-    .then(post =>{
-        if(post.length > 0){
-            return res.status(200).json(post);
-        } else {
-            return res.status(404).json({message: "Post with that id not found"});
-        }
-    })
-    .catch(err => res.status(500).json({errorMessage: "The posts information could not be retrieved."}))
-})
-
-server.delete('/api/posts/:id', (req, res) => {
-    db.remove(req.params.id)
-    .then(posts => {
-        if(posts){
-            db.findById(req.params.id).then(post=>{
-                res.status(200).json(post);
-            })
-            .catch(err=>{
-                res.status(500).json({errorMessage: "The post could not be removed"})
-            })
-            res.status(200).json({message: 'This post was YEETED'})
-        } else {
-            res.status(404).json({message: 'The post with the specified ID does not exist.' })
-        }
-    })
-});
-
-server.put('/api/posts/:id', (req, res) => {
-    const updatePost = req.body
-    if(updatePost.title == "" || updatePost.contents == ""){
-        return res.status(400).json({errorMessage: 'Title or contents missing. Please address and try again'})
-    } else {
-    db.update(req.params.id, updatePost)
-    .then(count => {
-        if(count){
-            db.findById(req.params.id).then(post=>{
-                res.status(200).json(post);
-            })
-            .catch(err=>{
-                res.status(500).json({errorMessage: "The post could not be updated"})
-            })
-            res.status(201).json({message: 'This post was updated'});
-        } else {
-            res.status(404).json({message: 'The post with the specified ID does not exist.' })
-        }
-    })
-    .catch(err => res.status(500).json({errorMessage: 'Something went wrong on our end'}))
-}})
-
-
-
-
 
 const port = 5000;
 server.listen(port, () => console.log(`*** Server listening on ${port} ***`))
